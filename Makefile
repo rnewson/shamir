65354f55713dcc37ae9510d509ffca41abb23093
Robert Newson <robert.newson@gmail.com>
2011-12-05 22:34:40 -0500

    add PropEr test

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
 ERL ?= erl
 APP := shamir
 
-all:
+all: build
+
+build: deps
 	@./rebar compile
 
 clean:
 	@./rebar clean
+
+deps: ./deps/
+	./rebar get-deps update-deps
 
 test: all
 	@(./rebar skip_deps=true eunit)
--- test/shamir_test.erl
+++ test/shamir_test.erl
@@ -14,6 +14,7 @@
 
 -module(shamir_test).
 -include("shamir.hrl").
+-include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 hello_test() ->
@@ -27,3 +28,46 @@ shamir(Secret, Threshold, Count) ->
     ?assertEqual(Count, length(lists:usort([Y || #share{y=Y} <- Shares]))),
     RecoveredSecret= shamir:recover(lists:sublist(Shares, Threshold)),
     ?assertEqual(Secret, RecoveredSecret).
+
+proper_test_() ->
+    PropErOpts = [
+                  {to_file, user},
+                  {max_size, 60},
+                  {numtests, 1000}
+                 ],
+    {timeout, 3600,
+     ?_assertEqual([], proper:module(shamir_test, PropErOpts))}.
+
+prop_secrets() ->
+    ?FORALL(Secret, secret(),
+            begin
+                Threshold = random:uniform(10),
+                Count = Threshold * random:uniform(10),
+                Shares = shamir:share(Secret, Threshold, Count),
+                RecoveredSecret = shamir:recover(
+                                    lists:sublist(
+                                      shuffle(Shares), Threshold)),
+                Secret =:= RecoveredSecret
+            end
+           ).
+
+secret() ->
+    crypto:rand_bytes(1 + random:uniform(64)).
+
+shuffle(List) ->
+    %% Determine the log n portion then randomize the list.
+    randomize(round(math:log(length(List)) + 0.5), List).
+
+randomize(1, List) ->
+    randomize(List);
+randomize(T, List) ->
+    lists:foldl(fun(_E, Acc) ->
+                        randomize(Acc)
+                end, randomize(List), lists:seq(1, (T - 1))).
+
+randomize(List) ->
+    D = lists:map(fun(A) ->
+                          {random:uniform(), A}
+                  end, List),
+    {_, D1} = lists:unzip(lists:keysort(1, D)),
+    D1.
